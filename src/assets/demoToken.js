d59352971ff8cad79a767fc2a2a5322763c82c68
Yian Chen <147808320+cya6b@users.noreply.github.com>
2025-06-18 16:32:03 +0800

    Add files via upload

---
+++ src/assets/demoToken.js
@@ -0,0 +1,27 @@
+const demoToken = {
+  // https://bimface.com/developer-jsdemo#1091
+  token2d: '',
+  // https://bimface.com/developer-jsdemo#940
+  token2d_2: '',
+  token2d_3: '',
+  // https://bimface.com/developer-jsdemo#1048
+  token2d_4: '',
+  // https://bimface.com/developer-jsdemo#1087
+  token3d: '',
+  // https://bimface.com/developer-jsdemo#1023
+  token3d_2: '',
+
+  // 模型和视图 https://bimface.com/developer-jsdemo#1023
+  token3d_3: '',
+  token3d_3_fileId: '',
+  token3d_3_sheetId: '',
+
+  // http://standard.release.tushenyun.com/  图模相符性演示项目 建筑 1F-首层平面图
+  overlying: {
+    token2d: '',
+    token3d: '',
+    sheetId: '',
+    matric: '[1.0000001316787128,0,0,0,0,1.0000001316787128,0,0,0,0,1,0,-262279.1241119653,120723.96686142143,0,1]'
+  },
+}
+export default demoToken
