4c7809fa3fe53b0bf61152ef70f71ba3990f0b93
Yian Chen <147808320+cya6b@users.noreply.github.com>
2025-06-18 16:24:30 +0800

    Add files via upload

---
+++ public/static/sdkList.js
@@ -0,0 +1,53 @@
+/* Copyright (C) <1998-2024> Glodon Company Limited
+ * Licensed under the MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+export default [
+  {
+    label: 'gda_js_sdk_uploader',
+    date: '2023-07-03',
+    path: 'gda-js-sdk_uploader.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.0.0',
+    date: '2023-07-03',
+    path: 'gda-js-sdk_1.0.0.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.1.0',
+    date: '2023-10-10',
+    path: 'gda-js-sdk_1.1.0.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.1.1',
+    date: '2023-10-16',
+    path: 'gda-js-sdk_1.1.1.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.1.3',
+    date: '2023-10-24',
+    path: 'gda-js-sdk_1.1.3.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.1.4',
+    date: '2023-11-04',
+    path: 'gda-js-sdk_1.1.4.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.1.5',
+    date: '2023-12-07',
+    path: 'gda-js-sdk_1.1.5.zip'
+  },
+  {
+    label: 'gda_js_sdk_1.2.0',
+    date: '2024-01-08',
+    path: 'gda-js-sdk_1.2.0.zip'
+  },
+  {
+    label: 'gda-js-sdk_pdfdiff_1.2.0',
+    date: '2024-01-08',
+    path: 'gda-js-sdk_pdfdiff_1.2.0.zip'
+  }
+]
